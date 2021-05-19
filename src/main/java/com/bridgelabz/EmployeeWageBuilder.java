1829d4abca730d733c60091e0ab618a7b238be16
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-19 13:49:51 +0530

    ADDED:functionality to check emp person or absenst

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,8 +1,16 @@
 package com.bridgelabz;
 
+import java.awt.*;
+
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        System.out.println("welcome to emp wage computation program");
-
+       int IS_PRESENT = 1;
+       double empCheck = Math.floor((Math.random() * 10)) % 2;
+        System.out.println(empCheck);
+       if(empCheck == IS_PRESENT) {
+           System.out.println("employ is present");
+       }else {
+           System.out.println("employ is absent");
+       }
     }
 }
